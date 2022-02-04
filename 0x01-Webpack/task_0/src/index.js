7b8eaf654455af32feca94d916d174e92ae8db8c
jfkquality <jfkquality@gmail.com>
2022-02-03 23:06:26 -0500

    581 t.0. [Bugfix?]. Simply index.js.

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,11 +1,5 @@
 import $ from "jquery";
 
-// import jquery and add three different paragraphs to the page body
-
-$(document).ready(() => {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
