57aca785c99c92c517171e9e673bcde09d584bc9
jfkquality <jfkquality@gmail.com>
2022-02-04 00:21:11 -0500

    581 t.2. [Bugfix]. Fix web config file. Add logo image to assets dir.

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,8 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./js/dashboard_main",
+  entry: "./js/dashboard_main.js",
   mode: "production",
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ]
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
