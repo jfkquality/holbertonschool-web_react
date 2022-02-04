c3260b43cb3194c53256e19697ea2f00002a15f0
jfkquality <jfkquality@gmail.com>
2022-02-04 00:40:03 -0500

    581 t.3 [Init commit].

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: "./public",
+    port: 8564
+  }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  mode: "development",
+  output: {
+    path:path.resolve(__dirname, "public"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ]
+      }
+    ]
+  }
+}
