a23c1f00bdc3b05c1903d6402fe047292106ff1a
jfkquality <jfkquality@gmail.com>
2022-02-08 22:12:44 -0500

    589 t.1. [Init Commit].

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// Task 1.
+
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+  }
+  const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false
+  };
+  
+  console.log(teacher3);
+  
