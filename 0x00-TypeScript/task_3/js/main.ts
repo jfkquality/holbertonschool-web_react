fccd4c5d72814be38f4b7ce851ddba545432ed10
jfkquality <jfkquality@gmail.com>
2022-02-08 22:33:33 -0500

    589 t.8. [Init Commit].

---
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -0,0 +1,17 @@
+/// <reference path ="crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+// console.log(newRowID);
+const updatedRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowID, updatedRow);
+console.log(updatedRow);
+CRUD.deleteRow(newRowID)
