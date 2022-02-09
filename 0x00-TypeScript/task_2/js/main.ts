f7e512ee91f94035f850e9b39e16d91e166852cf
jfkquality <jfkquality@gmail.com>
2022-02-08 22:30:38 -0500

    589 t.7. [Init Commit].

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -88,3 +88,19 @@ interface DirectorInterface {
   console.log(executeWork(createEmployee(200)));
   console.log(executeWork(createEmployee(1000)));
   
+  // Task 7.
+  
+  type Subjects = "Math" | "History";
+  
+  export function teachClass(todayClass: Subjects) {
+    if (todayClass === 'Math') {
+      return 'Teaching Math';
+    }
+    if (todayClass === 'History') {
+      return 'Teaching History';
+    }
+  }
+  
+  console.log(teachClass('Math'));
+  console.log(teachClass('History'));
+  
