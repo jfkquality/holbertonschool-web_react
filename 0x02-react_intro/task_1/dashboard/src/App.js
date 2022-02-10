b6654a11983f7f5915dc510774221125e4fc04d3
jfkquality <jfkquality@gmail.com>
2022-02-09 21:01:43 -0500

    596 t.1 [Feature] Embedding expressions, functions. Init commit.

---
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.jpg';
+import './App.css';
+import {getFooterCopy, getFullYear} from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
