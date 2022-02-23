22eda4a52b90c4d919ec48f896ab4da9aac99d3c
jfkquality <jfkquality@gmail.com>
2022-02-23 03:59:14 -0500

    596. t.7. [Init commit]. Deploy to a GitHub page

---
+++ 0x02-react_intro/task_4/dashboard/src/App.test.js
@@ -0,0 +1,22 @@
+import { shallow } from 'enzyme';
+import App from './App';
+
+// const wrapper = shallow(<App />);
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.exists);
+    });
+    it('renders a div with the class App-header', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find("header.App-header").length).toBe(1);
+    });
+    it('renders a div with the class App-body', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find("main.App-body").length).toBe(1);
+    });
+    it('renders a div with the class App-footer', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find("footer.App-footer").length).toBe(1);
+    });
+});
