88bb23fa9f6a1b6bd9769ffbe0c4a98297e61f5b
jfkquality <jfkquality@gmail.com>
2022-02-22 23:20:23 -0500

    596. t.5. Install Enzyme. [Init commit].

--- 0x02-react_intro/task_3/dashboard/src/App.test.js
+++ 0x02-react_intro/task_3/dashboard/src/App.test.js
@@ -1,19 +1,22 @@
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
 
-const wrapper = shallow(<App />);
+// const wrapper = shallow(<App />);
 describe('<App />', () => {
     it('renders without crashing', () => {
-        // shallow(<App />);
+        const wrapper = shallow(<App />);
         expect(wrapper.exists);
     });
     it('renders a div with the class App-header', () => {
-        expect(wrapper.find("div.App-header").length).toBe(1);
+        const wrapper = shallow(<App />);
+        expect(wrapper.find("header.App-header").length).toBe(1);
     });
     it('renders a div with the class App-body', () => {
-        expect(wrapper.find("div.App-body").length).toBe(1);
+        const wrapper = shallow(<App />);
+        expect(wrapper.find("main.App-body").length).toBe(1);
     });
     it('renders a div with the class App-footer', () => {
-        expect(wrapper.find("div.App-footer").length).toBe(1);
+        const wrapper = shallow(<App />);
+        expect(wrapper.find("footer.App-footer").length).toBe(1);
     });
 });
