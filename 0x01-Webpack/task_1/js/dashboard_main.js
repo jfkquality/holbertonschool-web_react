9dee521abe7356f072ef4dbc2662aaa045c6b1ca
jfkquality <jfkquality@gmail.com>
2022-02-23 22:47:58 -0500

    581. t.1. {Bugfix]. Fix debounce. Fish build script.

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -4,14 +4,22 @@ import _ from "lodash";
 $('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the studnts</p>");
 $('body').append("<button>Click here to get started</button>");
+updateCounter();
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
 
-let count = 0;
-
 function updateCounter() {
-  count = count + 1;
-  $('#count').text(`${count.toString()} clicks on the button`);
+  let count = 0;
+
+  $('button').on('click', _.debounce(() => {
+    count += 1;
+    $('#count').html(`${count} clicks on the button`)
+  }, 500));
 }
 
-$('button').click(_.debounce(updateCounter));
+// let count = 0;
+// function updateCounter() {
+//   count = count + 1;
+//   $('#count').text(`${count.toString()} clicks on the button`);
+// }
+// $('button').on('click(_.debounce(updateCounter)); click(_.debounce(updateCounter));
