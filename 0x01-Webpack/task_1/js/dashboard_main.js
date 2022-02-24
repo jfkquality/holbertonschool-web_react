fd2b9b813ce0e747c7e7240380d346fc246533c3
jfkquality <jfkquality@gmail.com>
2022-02-23 23:02:25 -0500

    581. t.1. {Bugfix]. Require imports.

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
-import $ from "jquery";
-import _ from "lodash";
+const $ = require( "jquery" );
+const _ = require('lodash');
 
 function updateCounter() {
   let count = 0;
@@ -10,16 +10,9 @@ function updateCounter() {
   }, 500));
 }
 
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the studnts</p>");
-$('body').append("<button>Click here to get started</button>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>')
 updateCounter();
-$('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
-
-// let count = 0;
-// function updateCounter() {
-//   count = count + 1;
-//   $('#count').text(`${count.toString()} clicks on the button`);
-// }
-// $('button').on('click(_.debounce(updateCounter)); click(_.debounce(updateCounter));
+$('body').append('<p id=count></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
