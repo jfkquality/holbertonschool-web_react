9d465da42732a558ceccaf758af0b5258771e5de
jfkquality <jfkquality@gmail.com>
2022-03-13 01:00:08 -0500

    608. React Immutable. T-5. [Bugfix]. Add toJS().

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,9 +1,29 @@
 import { List, Map } from 'immutable';
 
 export function concatElements(page1, page2) {
-  return List(page1).concat(List(page2));
+  return List(page1).concat(List(page2)).toJS();
 }
 
 export function mergeElements(page1, page2) {
-  return List(Map(page1).merge(Map(page2)));
+  return List(Map(page1).merge(Map(page2))).toJS();
 }
+
+// const obj1 =  {
+//     1: 'Liam',
+//     2: 'Noah',
+//     3: 'Elijah',
+//     4: 'Oliver',
+//     5: 'Jacob',
+//     6: 'Lucas',
+// };
+
+// const obj2=  {
+//     1: 'John',
+//     2: 'Noah',
+//     3: 'Elijah',
+//     4: 'Oliver',
+//     5: 'Jacob',
+//     6: 'Lucas',
+// };
+
+// console.log(mergeElements(obj1, obj2));  //.toJS());
