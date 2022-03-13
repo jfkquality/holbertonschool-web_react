ef224c08f4adb6da46adebab8e675d6716c5a217
jfkquality <jfkquality@gmail.com>
2022-03-12 23:27:33 -0500

    608. React Immutable. T-3. [Init commit].

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -3,7 +3,7 @@ import { fromJS } from 'immutable';
 export default function accessImmutableObject(object, array) {
   const name = fromJS(object);
   // return fromJS(getIn(object, array)); //Manny
-  return name.getIn(array, undefined);
+  return name.getIn(array, undefined); //Jonny
 }
 
 // console.log(accessImmutableObject({
