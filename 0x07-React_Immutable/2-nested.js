95c834c37dc91a5dd701a93103c0a5fac3440dc9
jfkquality <jfkquality@gmail.com>
2022-03-12 23:00:49 -0500

    608. React Immutable. T-2. [Bugfix/Refactor]. Change getIn format.

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -2,12 +2,13 @@ import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   const name = fromJS(object);
-  return name.getIn([array[0], array[1]]);
+  // return fromJS(getIn(object, array)); //Manny
+  return name.getIn(array, undefined);
 }
 
 // console.log(accessImmutableObject({
 //   name: {
-//        first: "Guillaume",
-//        last: "Salva"
-//   }
+//     first: 'Guillaume',
+//     last: 'Salva',
+//   },
 // }, ['name', 'first']));
